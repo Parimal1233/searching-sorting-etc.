f911a24b8d4ff93efd6f91cc401e837387947c87
Parimal1233 <142471379+Parimal1233@users.noreply.github.com>
2023-10-26 11:41:23 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int var1;
+    cout<<"Enter the age:";
+    cin>>var1;
+    string var2;
+    cout<<"Enter the name:";
+    cin>>var2;
+    float var3;
+    cout<<"Enter your GPA:";
+    cin>>var3;
+    char var4;
+    cout<<"Enter the grade:";
+    cin>>var4;
+
+    cout<<endl;
+
+
+    cout<<"MY age is "<<var1<<endl;
+    cout<<"My Name is "<<var2<<endl;
+    cout<<"MY GPA is "<<var3<<endl;
+    cout<<"The grade is "<<var4<<endl;
+    cout<<endl;
+
+// Size of the variable
+    // cout<<"Size of the variables are:"<<endl;
+    // cout<<sizeof(var1)<<endl;
+    // cout<<sizeof(var2)<<endl;
+    // cout<<sizeof(var3)<<endl;
+    // cout<<sizeof(var4)<<endl;
+}
