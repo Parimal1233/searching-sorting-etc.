5cc99b744e8cc939c059d9f8b93139d545a5be44
Parimal1233 <142471379+Parimal1233@users.noreply.github.com>
2023-10-26 11:42:35 +0530

    Add files via upload

---
+++ tut15.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    int arr[5] = {10,3,0,1,2};
+    int size = 5;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+}
